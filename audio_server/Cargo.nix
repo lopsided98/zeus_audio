608dc522690e7052801d7de9e300e1298df3938c
Ben Wolsieffer <benwolsieffer@gmail.com>
2022-11-13 18:17:54 -0500

    Add back linker hack after update.

--- audio_server/Cargo.nix
+++ audio_server/Cargo.nix
@@ -4650,7 +4650,7 @@ rec {
                       crateConfig.sha256;
                   }
                 );
-                extraRustcOpts = lib.lists.optional (targetFeatures != [ ]) "-C target-feature=${lib.concatMapStringsSep "," (x: "+${x}") targetFeatures}";
+                extraRustcOpts = [ "-C" "linker=${pkgs.stdenv.cc.targetPrefix}cc" ] ++ lib.lists.optional (targetFeatures != [ ]) "-C target-feature=${lib.concatMapStringsSep "," (x: "+${x}") targetFeatures}";
                 inherit features dependencies buildDependencies crateRenames release;
               }
             );
